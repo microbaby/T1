173d66dec9a24cd199839e75e4b454f71bd0727f
microbaby <patrickrafanan@me.com>
2016-04-24 18:23:29 -0400

    Update iframe.js

--- iframe.js
+++ iframe.js
@@ -1,16 +1,4 @@
-$(document).ready(function(){
+Load external JS from bookmarklet
+sample code via: https://stackoverflow.com/questions/106425/load-external-js-from-bookmarklet
 
-// sample of iframe for bookmarklet - via http://www.yottaa.com/company/blog/application-optimization/using-jquery-and-github-to-create-a-bookmarklet/
-
-//Need to pull off the "#" from the string
-	var data = window.location.hash.substring(1);
-
-	//if the string is empty that means there is no data and
-	//the browser does not support the API.
-	if (data != ""){
-		data = JSON.parse(data);
-		showW3cNavPerformanceData(data);
-	}else{
-		$(document.body).html($("#w3c-nav-bookmarklet-notsupported").render({}));
-	}
-});
+javascript:(function(){document.body.appendChild(document.createElement('script')).src='** your external file URL here **';})();
