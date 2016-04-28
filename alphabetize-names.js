53aa8ec06195c8e0f4aa0a57bc482c8bf3203f5c
microbaby <patrickrafanan@me.com>
2016-04-28 16:19:33 -0400

    Update alphabetize-names.js
    add comments and add'l code to do sorting

--- alphabetize-names.js
+++ alphabetize-names.js
@@ -1,4 +1,9 @@
 javascript:(function(){
   // alphabetize names
+  // expand people list
 $(".people .icon-open_circle").click();
+// sorts people list
+var items = $(".people .filters div")
+var new_items = _.sortBy(items, function(i) { return $(i).find("a").html(); });
+$(".people .filters").html(new_items);
 }());
