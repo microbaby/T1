d3216dd25c7131bc55287c6f136067e13c025704
microbaby <patrickrafanan@me.com>
2016-10-05 12:20:06 -0400

    reduce spacing between name fields

--- compress_name_list.js
+++ compress_name_list.js
@@ -7,24 +7,29 @@ by PATRICK RAFANAN for BFA keywording
 v.b1 created 06.09.2016
 */
 
-/* hide user column data */
+  /* hide user column data */
 $(".user_chooser").css({"display":"none"});
 
 /* form margins */
 /*$("form").css({"margin":"0"});*/
 
-/* extend name field width */
+  /* extend name field width */
 $("input").css({"width":"406px"});
   
+  /* reduce spacing between name fields */
+$("input").css({"margin-bottom":"0px"});
+$("input").css({"padding":"0px 3px 0px"});
+$("input").css({"height":"18px"});
+
   /* table padding */
 $(".table th, .table td").css({"padding":"0"});
-
-/* table width */
+  
+  /* table width */
 $(".table").css({"width":"0px"});
 
-/* table borders */
+  /* table borders */
 $(".table-bordered").css({"border":"1px solid green"});
 /*$(".table th, .table td").css({"border-top":"3px solid #00FF00"});*/
 /*$(".table-bordered th, .table-bordered td").css({"border-left":"3px solid blue"});*/
-
+  
 })();
